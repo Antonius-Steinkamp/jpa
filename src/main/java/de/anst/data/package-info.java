9c02ef212f002b87f1f588cff025a34b69318afd
start.vaadin.com <no-reply@vaadin.com>
2024-02-07 10:57:56 +0000

    Generated project

---
+++ src/main/java/de/anst/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package de.anst.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/de/anst/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package de.anst.services;
+
+import org.springframework.lang.NonNullApi;
